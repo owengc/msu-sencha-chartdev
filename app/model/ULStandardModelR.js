30f3566500427fbff2e2b340db3b71e69d8281d5
owengc <owen.campbell@gmail.com>
2013-07-25 13:15:25 -0400

    bringing back updated ULStandardModelR from TJDev

--- app/model/ULStandardModelR.js
+++ app/model/ULStandardModelR.js
@@ -4,69 +4,43 @@ Ext.define('app.model.ULStandardModelR', {
         fields: [
             {name: 'fullcode', type: 'string', 
 	     convert: function(value, record){
-		 var fullcode, code;
-		 if(value){
-		     fullcode=value.replace(/^\s+|\s+$/g, '') || code;
+		 if(value!=null){
+		     var fullcode=value.replace(/^\s+|\s+$/g, '');
+		     return fullcode || 'Other';
 		 }
-		 else{
-		     code=record.get('code');
-		     if(code){
-			 code=code.replace(/^\s+|\s+$/g, '');
-		     }
-		 }
-		 return fullcode || code || 'Other';
+		 return '';
 	     }
 	    },
 	    {name: 'code', type: 'string',
 	     convert: function(value, record){
-		 var code, fullcode;
-		 if(value){
-		     code=value.replace(/^\s+|\s+$/g, '');
+		 if(value!=null){
+		     var code=value.replace(/^\s+|\s+$/g, '');
+		     return code || 'Other';
 		 }
-		 else{
-		     fullcode=record.get('fullcode');
-		 }
-		 return code || fullcode || 'Other';
+		 return '';
 	     }
 	    },
 	    {name: 'frameworktitle', type: 'string',
 	     convert: function(value, record){
-		 var frameworktitle, description;
-		 if(value){
-		     frameworkTitle=value.replace(/^\s+|\s+$/g, '');
+		 if(value!=null){
+		     var frameworktitle=value.replace(/^\s+|\s+$/g, '');
+		     return frameworktitle || 'No description.';
 		 }
-		 else{
-		     description=record.get('description');
-		     if(description){
-			 description=description.replace(/^\s+|\s+$/g, '');
-		     }
-		 }
-		 return frameworktitle || description || 'No description.';
+		 return '';
 	     }
 	    },
 	    {name: 'description', type: 'string',
 	     convert: function(value, record){
-		 var description, frameworktitle;
-		 if(value){
-		     description=value.replace(/^\s+|\s+$/g, '');
+		 if(value!=null){
+		     var description=value.replace(/^\s+|\s+$/g, '');
 		 }
-		 else{
-		     frameworktitle=record.get('frameworktitle');
-		     if(frameworktitle){
-			 frameworktitle=frameworktitle.replace(/^\s+|\s+$/g, '');
-		     }
-		 }
-		 return description || frameworktitle || 'No description.';
+		 return '';
 	     }
 	    },
 	    {name: 'duration_mask', type: 'string'},
             {name: 'percent', type: 'int'},
             {name: 'framework_id', type: 'int'},
-	    {name: 'standard_id', type: 'int',
-	     convert: function(value, record){
-		 return record.get('framework_id');
-	     }
-	    }
+	    {name: 'standard_id', type: 'int'}
         ]
     }
 });
